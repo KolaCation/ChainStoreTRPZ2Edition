ba2f84f6b5982e2069d922f82adb861419ea7d23
KolaCation <arharovoversteam@gmail.com>
2020-11-27 12:23:47 +0200

    LoginPage, RegisterPage, MainViewModel, BaseViewModel, RelayCommand

---
+++ ChainStoreTRPZ2Edition/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using ChainStoreTRPZ2Edition.Pages.Account;
+
+namespace ChainStoreTRPZ2Edition.ViewModels
+{
+    public sealed class MainViewModel : BaseViewModel
+    {
+        private Page _currentPage;
+
+        public Page CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+            }
+        }
+
+ 
+        public ICommand OpenLoginPage { get; set; }
+        public ICommand OpenRegisterPage { get; set; }
+
+        public MainViewModel()
+        {
+            //Create command
+            OpenLoginPage = new RelayCommand(() => {
+                CurrentPage = new LoginPage();
+            });
+            OpenRegisterPage = new RelayCommand(() =>
+            {
+                CurrentPage = new RegisterPage();
+            });
+        }
+    }
+}
---
+++ ChainStoreTRPZ2Edition/ViewModels/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ChainStoreTRPZ2Edition.ViewModels
+{
+    public sealed class RelayCommand : ICommand
+    {
+        #region Private Members
+        private Action _action;
+        #endregion
+
+        #region Public events
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+        #endregion
+
+        #region Conctructor
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+        #endregion
+
+        #region Command methods
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+        #endregion
+    }
+}
+
