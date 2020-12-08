a4fbf59436b45ad33aacd82e5b76a5ee164f1e93
KolaCation <arharovoversteam@gmail.com>
2020-12-08 18:27:32 +0200

    StoreDetails: view, filter.

--- ChainStoreTRPZ2Edition/ViewModels/Stores/StoreDetailsViewModel.cs
+++ ChainStoreTRPZ2Edition/ViewModels/Stores/StoreDetailsViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 using ChainStore.DataAccessLayer.Identity;
 using ChainStore.DataAccessLayer.Repositories;
 using ChainStore.Domain.DomainCore;
@@ -15,43 +18,98 @@ namespace ChainStoreTRPZ2Edition.ViewModels.Stores
     {
         #region Properties
 
-        public Store Store { get=>GetValue<Store>(); set=>SetValue(value); }
-        public string SearchProduct { get=>GetValue<string>(); set=>SetValue(value); }
+        public Store Store
+        {
+            get => GetValue<Store>();
+            set => SetValue(value);
+        }
+
+        public ObservableCollection<Category> Categories { get; set; }
+
+        public string SearchProduct
+        {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+
         private readonly IAuthenticator _authenticator;
         private readonly IStoreRepository _storeRepository;
 
         #endregion
 
         #region Commands
+
+        public ICommand Filter { get; set; }
+        public ICommand ClearFilter { get; set; }
+
         #endregion
 
         #region Contsructor
+
         public StoreDetailsViewModel(IAuthenticator authenticator, IStoreRepository storeRepository)
         {
             _authenticator = authenticator;
             _storeRepository = storeRepository;
+            Categories = new ObservableCollection<Category>();
             Messenger.Default.Register<RefreshDataMessage>(this, RefreshDataAsync);
+            Filter = new RelayCommand(HandleFiltering);
+            ClearFilter = new RelayCommand(HandleCleaning);
         }
+
         #endregion
 
         #region Methods
+
         public async void RefreshDataAsync(RefreshDataMessage refreshDataMessage)
         {
             if (GetType().Name.Equals(refreshDataMessage.ViewModelName) && refreshDataMessage.ItemId != null)
             {
+                Categories.Clear();
                 var store = await _storeRepository.GetOne(refreshDataMessage.ItemId.Value);
                 Store = store;
+                foreach (var category in store.Categories)
+                {
+                    Categories.Add(category);
+                }
             }
         }
 
         public void ClearData()
         {
+            Categories.Clear();
+            SearchProduct = string.Empty;
+            Store = null;
         }
+
         #endregion
 
         #region Handlers
+
+        private void HandleFiltering()
+        {
+            RefreshDataAsync(new RefreshDataMessage(GetType().Name, Store.Id));
+            var categoriesToDisplay = new List<Category>();
+            if (!string.IsNullOrEmpty(SearchProduct))
+            {
+                categoriesToDisplay.AddRange(Store.Categories.Where(category =>
+                    category.Products.Any(e => e.Name.ToLower().Contains(SearchProduct.ToLower()))));
+                Categories.Clear();
+                foreach (var category in categoriesToDisplay)
+                {
+                    var productsToDisplay = category.Products
+                        .Where(product => product.Name.ToLower().Contains(SearchProduct.ToLower())).ToList();
+                    var result = new Category(productsToDisplay, category.Id, category.Name);
+                    Categories.Add(result);
+                }
+            }
+        }
+
+        private void HandleCleaning()
+        {
+            SearchProduct = string.Empty;
+            RefreshDataAsync(new RefreshDataMessage(GetType().Name, Store.Id));
+        }
+
         #endregion
-
-
     }
 }
